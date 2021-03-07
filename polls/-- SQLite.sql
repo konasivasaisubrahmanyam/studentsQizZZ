efc922cffa0632cfc0634dd55ad7b2a214f64733
Kona siva sai subrahmanyam <47383419+konasivasaisubrahmanyam@users.noreply.github.com>
2021-03-07 18:13:04 +0530

    Initial commit

---
+++ polls/-- SQLite.sql
@@ -0,0 +1,10 @@
+-- SQLite
+
+--SELECT * FROM  auth_user;
+ --\dt (PostgreSQL), SHOW TABLES;
+
+
+ INSERT into auth_user(id,password,last_login,is_superuser,username,last_name,email,is_staff,is_active,date_joined,first_name) VALUES (114,'sateesh@311', '02/feb/2021',true,'saiii','y','sai.y@gmail.com', true, true, 2021-03-05, 'sateeshh');
+ --CREATE table auth_user(username varchar(128), password varchar(128))
+
+
